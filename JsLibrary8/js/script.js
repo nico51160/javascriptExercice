e7f2433c0c7787305e4b10ecaff7a27847d93914
unknown <nicolas.terry@hotmail.fr>
2023-04-12 01:05:48 +0200

    newUpdate

--- JsLibrary8/js/script.js
+++ JsLibrary8/js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+    $('#boxPass').hide();
 
+    $('#user').change(function() {
+        if($(this).val() != '') {
+            $('#boxPass').show('slow');
+            $(this).attr('class', 'on');
+        } else {
+            $(this).attr('class', '');
+
+        }
+    });
+
+    $('#pass').keyup(function() {
+        if($(this).val().length == 6) {
+            $('input[type="submit"]').prop('disabled', false);
+            $(this).attr('class', 'on');
+            $('label').attr('class', 'vert');
+            $('section').attr('class', 'active');
+        } else {
+            $('input[type="submit"]').prop('disabled', true);
+            $(this).attr('class', '');
+            $('label').attr('class', '');
+            $('section').attr('class', '');
+        }
+    });
 });
