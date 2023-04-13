7a6b40566ea0d58317219fd0b8b9ecf0550f9a52
unknown <nicolas.terry@hotmail.fr>
2023-04-14 01:18:04 +0200

    newUpdate

--- JsLibrary9/js/script.js
+++ JsLibrary9/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $('input[name="produit"]').change(function() {
+        let produit = ($(this).attr('id'));
+        if(produit == 'boulangerie') {
+            $('.tarif').filter('.boulangerie').css('filter', 'blur(0)');
+            $('.tarif').filter('.patisserie').css('filter', 'blur(3px)');
+        } else {
+            $('.tarif').filter('.patisserie').css('filter', 'blur(0)');
+            $('.tarif').filter('.boulangerie').css('filter', 'blur(3px)');
+        }
+    });
 });
